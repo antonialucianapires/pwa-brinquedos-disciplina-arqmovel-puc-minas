e9c6048cdf7a7715ac489beacf2d8616e41a44f3
Antonia Luciana Pires <antonialucianapires@gmail.com>
2021-10-31 01:38:20 -0300

    ajustes na interface + card de brinquedos

--- js/app.js
+++ js/app.js
@@ -25,10 +25,52 @@ ajax.onreadystatechange = function () {
         if (datajson[i].brinquedos.length == 0) {
           html_content +=
             '<div class="alert alert-warning" role="alert">Desculpe. ainda não temos brinquedos cadastrados.</div>';
+        } else {
+          html_content += '<div class="row">';
+          for (let j = 0; j < datajson[i].brinquedos.length; j++) {
+            html_content += card_brinquedo(
+              datajson[i].brinquedos[j].nome,
+              datajson[i].brinquedos[j].imagem,
+              datajson[i].brinquedos[j].valor,
+              datajson[i].brinquedos[j].whatsapp
+            );
+          }
+
+          html_content += "</div>";
         }
       }
 
       content.innerHTML = html_content;
     }
   }
+};
+
+//template do card_brinquedo
+var card_brinquedo = function (nome, imagem, valor, whatsapp) {
+  return (
+    '<div class="col-lg-6">' +
+    '<div class="card">' +
+    '<img src="' +
+    imagem +
+    '" class="card-img-top" alt="' +
+    nome +
+    '">' +
+    '<div class="card-body">' +
+    '<h5 class="card-title">' +
+    nome +
+    "</h5>" +
+    '<p class="card-text"><strong>Valor:</strong> ' +
+    valor +
+    "</p>" +
+    '<div class="d-grid gap-2">' +
+    '<a href="https://api.whatsapp.com/send?phone=55' +
+    whatsapp +
+    "&text=Olá gostaria de informações sobre o brinquedo: " +
+    nome +
+    '" target="_blank" class="btn btn-info">Contato Proprietário</a>' +
+    "</div>" +
+    "</div>" +
+    "</div>" +
+    "</div>"
+  );
 };
